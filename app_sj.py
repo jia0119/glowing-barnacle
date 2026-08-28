53ca937c75469cca462e2b1834eeb6b2e7b23b17
jia0119 <limsj-wp24@student.tarc.edu.my>
2026-08-29 02:10:18 +0800

    Add files via upload

---
+++ app_sj.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "85aac671-bf84-456c-ac1f-582364a4dc7e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
